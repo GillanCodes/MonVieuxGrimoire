05455fb615d58884067ebbf9903b024f763c82e4
gillancodes <gillancodes@laposte.net>
2024-07-04 21:53:13 +0200

    fix: stack bearer

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -2,7 +2,6 @@ const jwt = require('jsonwebtoken')
 
 module.exports = (req, res, next) => {
 	try {
-		console.log(req.headers.authorization);
 		const token = req.headers.authorization.split(' ')[1]
 		const decodedToken = jwt.verify(token, process.env.JWT_TOKEN)
 		const userId = decodedToken.userId
