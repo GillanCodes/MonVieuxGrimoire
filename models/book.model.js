738b61ff2e1f087c64ef1ac26d1e6b38ae82c502
gillancodes <gillancodes@laposte.net>
2024-07-05 11:15:31 +0200

    fix: rating -> ratings

--- models/book.model.js
+++ models/book.model.js
@@ -25,7 +25,7 @@ const bookSchema = new mongoose.Schema({
         required: true,
         type: String,
     },
-    rating: {
+    ratings: {
         type: [
             {
                 userId: String,
