b2b86458b277e70e66a754ab9b0c86e2b510f321
gillancodes <gillancodes@laposte.net>
2024-07-08 11:16:28 +0200

    refactor: remove useless stuff

--- middlewares/multer.middleware.js
+++ middlewares/multer.middleware.js
@@ -1,22 +1,6 @@
 const multer = require('multer');
 const sharpMulter = require('sharp-multer');
 
-const MIME_TYPE = {
-	'image/jpg': 'jpg',
-	'image/jpeg': 'jpg',
-	'image/png': 'png',
-	'image/webp': 'webp',
-}
-
-// const newFilenameFunction = (og_filename, options) => {
-// 	const newname =
-// 	  og_filename.split(".").slice(0, -1).join(".") +
-// 	  `${options.useTimestamp ? "-" + Date.now() : ""}` +
-// 	  "." +
-// 	  options.fileFormat;
-// 	return newname;
-// };
-
 const newFilenameFunction = (og_filename, options) => {
 	const filename = og_filename.split(' ').join('_')
 	const filenameArray = filename.split('.')
@@ -26,20 +10,6 @@ const newFilenameFunction = (og_filename, options) => {
 	const newName = filenameWithoutExtention + Date.now() + "." + options.fileFormat
 	return newName
 }
-
-// const storage = multer.diskStorage({
-// 	destination: function (req, file, callback) {
-// 		callback(null, './public/books')
-// 	},
-// 	filename:  (req, file, callback) => {
-// 		const filename = file.originalname.split(' ').join('_')
-// 		const filenameArray = filename.split('.')
-// 		filenameArray.pop()
-// 		const filenameWithoutExtention = filenameArray.join('.')
-// 			const extension = MIME_TYPE[file.mimetype]
-// 		callback(null, filenameWithoutExtention + Date.now() + '.' + extension)
-// 	}
-// })
 
 const storage = sharpMulter({
 	destination: function (req, file, callback) {
@@ -52,7 +22,6 @@ const storage = sharpMulter({
 	},
 	filename: newFilenameFunction
 })
-
 
 const upload = multer({
 	storage,
